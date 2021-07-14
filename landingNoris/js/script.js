9c92fb5c043eeb01a94daf1703fc7d533bab0349
viktoriia-ko <viktoriia.sobolewski@gmail.com>
2021-07-14 12:58:59 +0300

    noris adapt

--- landingNoris/js/script.js
+++ landingNoris/js/script.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-  $(".slider").slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2500,
-  });
-
   $(".hamburger-menu").click(function (event) {
     $(".hamburger-menu,.menu").toggleClass("active");
     $("body").toggleClass("lock");
