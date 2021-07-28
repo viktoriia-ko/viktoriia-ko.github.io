3cb515a67d7cbf789eaed4a1ee38d235a227cd96
viktoriia-ko <viktoriia.sobolewski@gmail.com>
2021-07-28 20:46:36 +0300

    noris

--- landingNoris/js/script.js
+++ landingNoris/js/script.js
@@ -4,3 +4,20 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
 });
+
+function openProd(evt, prodName) {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(prodName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
